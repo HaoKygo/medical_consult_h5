e00e4364013ec9759ad5782d6fdcb4734a261fe6
Youhao Tang <tangyouhao0@gmail.com>
2023-04-13 21:25:45 +0800

    Build.

---
+++ build/assets/use-id-b3b21262.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-f5f1119c.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
---
+++ build/assets/useShowPrescription-23ca0c84.js
@@ -0,0 +1,1 @@
+import{i as o}from"./consult-df834d4c.js";import{s as i}from"./function-call-36cfbf3d.js";const c=()=>({showPrescription:async r=>{if(r){const s=await o(r);i([s.url])}}});export{c as u};
